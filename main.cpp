c6f1d57a324295ae5411646e971e1318ae0fcd4a
Talesb <talesbatistafig@hotmail.com>
2022-03-19 19:34:56 -0300

    Iniciando desenvolvimento das operações

--- main.cpp
+++ main.cpp
@@ -4,14 +4,31 @@ using namespace std;
 
 int main()
 {
+
+    int operacao;
     float  num1 , num2 , resultado;
+
+    cout<<"Digite a operação selecionada:"<<endl;
+    cout<<"1 - Soma"<<endl;
+    cout<<"2 - Subtracao"<<endl;
+    cout<<"3 - Divisao"<<endl;
+    cout<<"4 - Multiplicacao"<<endl;
+    cout<<"                 "<<endl;
+    cin>>operacao;
+
+    if(operacao<1 || operacao>4){
+        cout<<"Não existe a operação informada!";
+        return 0;
+    }
 
     cout << "digite o primeiro numero:";
     cin >> num1;
     cout <<"digite o segundo numero:";
     cin >> num2;
-    resultado = num1 + num2;
-    cout << " a soma e " << resultado;
+
+    // TODO Gustavo terminar
+
+    cout << "O Resultado é " << resultado;
 
 
     return 0;
